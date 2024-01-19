d1625aaa6ab2ba7f9d3253d5da0678782d5d3e13
Rushabh <rushabhsavaliya5@gnail.com>
2024-01-19 18:18:27 +0530

    c lauguage

---
+++ RUSHABH4 - Copy.C
@@ -0,0 +1,14 @@
+#include<stido.h>
+#include<conio.h>
+main(){
+clrscr("*\n");
+printf("*\n");
+printf("*\n");
+printf("*      * *     *\n");
+printf("*     *   *    *\n");
+printf("*    *     *   * \n");
+printf("");
+printf();
+printf();
+getch();
+}
